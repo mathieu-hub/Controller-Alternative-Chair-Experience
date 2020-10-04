af3706e260652a70d91ab4a80272d5432d587aaf
Aurélien <orelien.mary@gmail.com>
2020-10-04 17:33:52 +0200

    Setup Dossier + Movement

---
+++ Assets/AUM/AUM_Scripts/PScripts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PScripts : MonoBehaviour
+{
+    public SpriteRenderer sr;
+
+    public Movement movement;
+}
