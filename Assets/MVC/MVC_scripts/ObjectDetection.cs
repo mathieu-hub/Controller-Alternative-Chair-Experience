ed722f9f255a8c4b51d9f1b8611be4d24ad07497
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-10-06 16:53:20 +0200

    ObjectDetection in works (Add TargetableObjects and ObjectDetection Script)

---
+++ Assets/MVC/MVC_scripts/ObjectDetection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ObjectDetection : MonoBehaviour
+{
+    [SerializeField] private GameObject resultBar;
+    [SerializeField] private GameObject validationBar;
+    [SerializeField] private GameObject refuseBar;
+
+    private void Start()
+    {
+        resultBar = null;
+    }
+
+    private void Update()
+    {
+        if (gameObject.GetComponentInParent<TargetableObjects>().objectTargeted == gameObject.GetComponentInParent<TargetableObjects>().objectToTarget)
+        {
+            resultBar = validationBar; 
+        }
+        else
+        {
+            resultBar = refuseBar;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TargetableObject"))
+        {
+            gameObject.GetComponentInParent<TargetableObjects>().objectTargeted = gameObject;
+            StartCoroutine(LoadBar());
+        }        
+    }
+
+    IEnumerator LoadBar()
+    {
+        yield return new WaitForSeconds(0.5f);
+        //Jauge jaune qui augmente
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(ResultBarDisplay());
+    }
+
+    IEnumerator ResultBarDisplay()
+    {
+        resultBar.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        resultBar.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        resultBar.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        resultBar.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        resultBar.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        resultBar.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+    }
+}
---
+++ Assets/MVC/MVC_scripts/TargetableObjects.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetableObjects : MonoBehaviour
+{
+    //Object
+    public GameObject objectToTarget;
+    public GameObject objectTargeted;
+}
