4dde4bf9d64e71579990534608c0bf984d7978b6
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-10-08 17:16:02 +0200

    Fix Color Selection and bar setup/changement

--- Assets/MVC/MVC_scripts/TargetableObject.cs
+++ Assets/MVC/MVC_scripts/TargetableObject.cs
@@ -7,54 +7,16 @@ public class TargetableObject : MonoBehaviour
     //Object
     [Header("Object")]
     public GameObject objectToTarget;
-    public GameObject objectTargeted;
+    public GameObject objectTargeted;  
 
-    //Bar selected
-    [Header("Bar Selected")]
-    public GameObject barSelected;
-    public GameObject valBarSelected;
-    public GameObject refBarSelected;
-
-    //Bar Reference
-    [Header("BAR REFERENCES")]
-    [Header("Bar")]
-    [SerializeField] private GameObject bar01;
-    [SerializeField] private GameObject bar02;
-    [SerializeField] private GameObject bar03;
-    [SerializeField] private GameObject bar04;
-    [Header("Validation Bar")]
-    [SerializeField] private GameObject valBar01;
-    [SerializeField] private GameObject valBar02;
-    [SerializeField] private GameObject valBar03;
-    [SerializeField] private GameObject valBar04;
-    [Header("Refuse Bar")]
-    [SerializeField] private GameObject refBar01;
-    [SerializeField] private GameObject refBar02;
-    [SerializeField] private GameObject refBar03;
-    [SerializeField] private GameObject refBar04;
-
-    void Update()
+    private void Start()
     {
-        if (barSelected == bar01)
-        {
-            valBarSelected = valBar01;
-            refBarSelected = refBar01;
-        }
-        else if(barSelected == bar02)
-        {
-            valBarSelected = valBar02;
-            refBarSelected = refBar02;
-        }
-        else if (barSelected == bar03)
-        {
-            valBarSelected = valBar03;
-            refBarSelected = refBar03;
-        }
-        else if (barSelected == bar04)
-        {
-            valBarSelected = valBar04;
-            refBarSelected = refBar04;
-        }
+        objectTargeted = null;
+    }
+
+    private void Update()
+    {
+        print(objectTargeted);
     }
 }
 
