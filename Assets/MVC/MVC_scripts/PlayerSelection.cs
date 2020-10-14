02fa7ed0adef701cab37a2e50b730a5922e6b738
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-10-14 12:46:36 +0200

    Add Player Selection + Clean Hierarchy

---
+++ Assets/MVC/MVC_scripts/PlayerSelection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour
+{
+    public bool isPlayerOne;
+    public bool isPlayerTwo;
+}
