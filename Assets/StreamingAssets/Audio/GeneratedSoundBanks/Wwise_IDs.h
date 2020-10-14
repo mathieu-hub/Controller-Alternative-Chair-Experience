a705a156a7e091d83017be50086f4139c4af29d9
Théo <t.valet@rubika-edu.com>
2020-10-14 17:01:41 +0200

    Add Sound Part2 (Simon and Space Pong)

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,13 +13,18 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID APPARITION_BALLE = 4140142695U;
         static const AkUniqueID APPARITION_BOUTON_START = 1481623233U;
+        static const AkUniqueID ASTEROIDE_EXPLOSION = 119072111U;
+        static const AkUniqueID BALLE_BOUCLIER = 2087612307U;
+        static const AkUniqueID BALLE_MUR = 3963030902U;
         static const AkUniqueID CHARGEMENT_COULEUR = 1497809451U;
         static const AkUniqueID COULEUR_BLEU = 2968600283U;
         static const AkUniqueID COULEUR_JAUNE = 755896502U;
         static const AkUniqueID COULEUR_ROUGE = 2716105235U;
         static const AkUniqueID COULEUR_VERTE = 4126544963U;
         static const AkUniqueID ECHEC_COULEUR = 889509821U;
+        static const AkUniqueID EXPLOSION_VAISSEAU = 3198498328U;
         static const AkUniqueID VALIDATION_COULEUR = 52329734U;
     } // namespace EVENTS
 
