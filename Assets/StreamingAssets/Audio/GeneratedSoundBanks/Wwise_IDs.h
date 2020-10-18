1171afb67526a38a1d390f0388c2eee55dcf3f15
Théo <t.valet@rubika-edu.com>
2020-10-18 16:10:52 +0200

    Add Sound Part3

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,18 +13,28 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID ACCELERATEUR_BALLE = 2784276230U;
+        static const AkUniqueID ACTIVATION_BOUCLIER = 2479721409U;
         static const AkUniqueID APPARITION_BALLE = 4140142695U;
         static const AkUniqueID APPARITION_BOUTON_START = 1481623233U;
+        static const AkUniqueID APPLAUDISSEMENTS = 408738931U;
         static const AkUniqueID ASTEROIDE_EXPLOSION = 119072111U;
         static const AkUniqueID BALLE_BOUCLIER = 2087612307U;
         static const AkUniqueID BALLE_MUR = 3963030902U;
+        static const AkUniqueID CASSE_BRIQUE = 302720131U;
         static const AkUniqueID CHARGEMENT_COULEUR = 1497809451U;
         static const AkUniqueID COULEUR_BLEU = 2968600283U;
         static const AkUniqueID COULEUR_JAUNE = 755896502U;
         static const AkUniqueID COULEUR_ROUGE = 2716105235U;
         static const AkUniqueID COULEUR_VERTE = 4126544963U;
+        static const AkUniqueID DEDOUBLEMENT_BALLE = 281341014U;
         static const AkUniqueID ECHEC_COULEUR = 889509821U;
         static const AkUniqueID EXPLOSION_VAISSEAU = 3198498328U;
+        static const AkUniqueID EXTRABOUCLIER = 3159408660U;
+        static const AkUniqueID EXTRALIFE = 2800659499U;
+        static const AkUniqueID METEORITE = 13339075U;
+        static const AkUniqueID PASSAGE_ESPACE = 1893821891U;
+        static const AkUniqueID RENFORCEMENT = 3890001413U;
         static const AkUniqueID VALIDATION_COULEUR = 52329734U;
     } // namespace EVENTS
 
