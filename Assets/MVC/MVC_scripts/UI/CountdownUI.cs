4412eb58e507a74aeba62fc9f622e11d42723643
mathieu-hub <m.vancappellen@rubika-edu.com>
2020-10-19 14:55:08 +0200

    Fix UI + Add CountdownUI

---
+++ Assets/MVC/MVC_scripts/UI/CountdownUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownUI : MonoBehaviour
+{
+    Text countdownUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdownUI = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdownUI.text = SimonGameManager.sgm.countdown + "";
+    }
+}
