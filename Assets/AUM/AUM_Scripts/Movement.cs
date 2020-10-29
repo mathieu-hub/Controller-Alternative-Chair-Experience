e3869c6f1a1de670e8310b116ce82046802cc69c
Unknown <orelien.mary@gmail.com>
2020-10-29 16:09:45 +0100

    Last Serre PC Push

--- Assets/AUM/AUM_Scripts/Movement.cs
+++ Assets/AUM/AUM_Scripts/Movement.cs
@@ -6,23 +6,17 @@ public class Movement : MonoBehaviour
 {
     public PScripts pScripts;
 
-    Transform vuforiaPlayerRef;
-
-    public ReadEncoder reRef;
+    ReadEncoder reRef;
 
     private void Start()
     {
-        vuforiaPlayerRef = GameObject.Find("Player" + pScripts.playerNumber + "Ref").transform;
+        reRef = GameObject.Find("Player" + pScripts.playerNumber + "Ref").GetComponent<ReadEncoder>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = new Vector3(vuforiaPlayerRef.position.x, transform.position.y, vuforiaPlayerRef.position.z);
-
         float rotationValue = (reRef.encoderValue % 90) * (360/90); 
-
-        float zRotation = vuforiaPlayerRef.rotation.eulerAngles.z;
 
         transform.localRotation = Quaternion.Euler(0f, 0f, rotationValue);
     }
